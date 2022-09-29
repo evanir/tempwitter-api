60f844ba8a5e2abba06e69d1e7079847146f74f6
Evanir de Oliveira Junior <evanir.jr@gmail.com>
2022-09-29 10:25:27 -0300

    Configurado Twitter API Client

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :twitter do
-        get '/auth/twitter2/callback', to: 'sessions#create'
+        get 'callback', to: 'sessions#create'
         post :tweet_forecast
         get :tweet_forecast
         #get :callback
