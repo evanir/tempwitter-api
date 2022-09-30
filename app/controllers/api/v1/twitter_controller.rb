e1f8e7900733a6e6373fbcb7853c370c0e30394f
Evanir de Oliveira Junior <evanir.jr@gmail.com>
2022-09-29 22:11:56 -0300

    criado tweet_forecast e adicao de codigo de retorno nos controllers

--- app/controllers/api/v1/twitter_controller.rb
+++ app/controllers/api/v1/twitter_controller.rb
@@ -1,11 +1,12 @@
 module Api::V1
   class TwitterController < ApplicationController
+
     def tweet_forecast
       city_name = I18n.transliterate(params[:city_name] || 'Brasilia')
-      forecast = Openweathermap::Sdk::Client.new({city_name: city_name})
-      twitter_client = TwitterClient.new().client
-      twitter_client.update("Testando ... #{city_name} #{Time.zone.now}")
-      # render json: twitter_client.response.to_json
+      city_id = params[:city_id]
+      tweet_forecast = TweetForecast.new({city_id: city_id, city_name: city_name})
+      tweet_forecast.publish_summary
+      render json: { message: tweet_forecast.status_message }, status: tweet_forecast.status_code
     end
 
     def callback
